0a2c275194db51fe9c8853403e51fd2de654e107
Christopher Nethercott <ccnethercott@gmail.com>
2020-04-28 16:31:04 +0100

    Removed username / auth from Accessory as it is stored in MiHomePlatform

--- src/accessory/plug.ts
+++ src/accessory/plug.ts
@@ -11,14 +11,12 @@ export class MiHomePlug {
 	constructor(
 		private readonly platform: MiHomePlatform,
 		private readonly accessory: PlatformAccessory,
-		private readonly username: string,
-		private readonly api_key: string,
-		private readonly id: string,
 	) {
 		this.accessory.getService(this.platform.Service.AccessoryInformation)!
 			.setCharacteristic(this.platform.Characteristic.Manufacturer, 'Energinie')
 			.setCharacteristic(this.platform.Characteristic.Model, "Default")
 			.setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+
 
 
 		this.service = this.accessory.getService(this.platform.Service.Switch) ?? this.accessory.addService(this.platform.Service.Lightbulb);
